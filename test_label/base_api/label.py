417d6b2ae6a1a26d593909d7213d832551be8f94
15823596020 <519312875@qq.com>
2020-06-15 11:13:22 +0800

    requests

--- test_label/base_api/label.py
+++ test_label/base_api/label.py
@@ -2,11 +2,14 @@ from test_requests.test_label.base_api.wework import WeWork
 
 
 class Label(WeWork): # 继承WeWork类
+    def __init__(self):
+        self.data = self.yaml_load("../data/config.yaml")
+
     #  创建标签，create_label_tagid方法，需传入参数tagname, tagid
     def create_label_tagid(self, tagname, tagid):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/create",
+            "method": self.data["create_label"]["method"],  #"post",
+            "url": self.data["create_label"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/create",
             "params": {"access_token": self.token},
             "json":{"tagname": tagname, "tagid": tagid}
         }
@@ -17,8 +20,8 @@ class Label(WeWork): # 继承WeWork类
     #  创建标签，create_label方法，需传入参数tagname；不传入tagid，因为tagid不是必填项，不指定时则以目前最大的id自增
     def create_label(self, tagname):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/create",
+            "method": self.data["create_label"]["method"], #"post",
+            "url": self.data["create_label"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/create",
             "params": {"access_token": self.token},
             "json": {"tagname": tagname}
         }
@@ -29,8 +32,8 @@ class Label(WeWork): # 继承WeWork类
     # 更新标签名字，update_label方法，需传入参数tagid, tagname
     def update_label(self, tagid, tagname):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/update",
+            "method": self.data["update_label"]["method"],  #"post",
+            "url": self.data["update_label"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/update",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "tagname": tagname}
         }
@@ -41,8 +44,8 @@ class Label(WeWork): # 继承WeWork类
     # 删除标签，delete_label方法，需传入参数tagid
     def delete_label(self, tagid):
         req = {
-            "method": "get",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/delete",
+            "method": self.data["delete_label"]["method"],  #"get",
+            "url": self.data["delete_label"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/delete",
             "params": {"access_token": self.token, "tagid": tagid}
         }
         r = self.send_api(req)  # 调用send_api方法，并传入一个json结构体的请求信息
@@ -52,8 +55,8 @@ class Label(WeWork): # 继承WeWork类
     # 获取标签成员，get_labelmember方法，需传入参数tagid
     def get_labelmember(self, tagid):
         req = {
-            "method": "get",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/get",
+            "method": self.data["get_labelmember"]["method"],  #"get",
+            "url": self.data["get_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/get",
             "params": {"access_token": self.token, "tagid": tagid}
         }
         r = self.send_api(req)  # 调用send_api方法，并传入一个json结构体的请求信息
@@ -63,8 +66,8 @@ class Label(WeWork): # 继承WeWork类
     # 增加标签成员，create_labelmember_all方法，需传入参数tagid,userlist,partylist
     def create_labelmember_all(self,tagid,userlist,partylist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
+            "method": self.data["create_labelmember"]["method"],  #"post",
+            "url": self.data["create_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
             "params": {"access_token": self.token},
             "json" : {"tagid": tagid,"userlist": userlist,"partylist": partylist}
         }
@@ -75,8 +78,8 @@ class Label(WeWork): # 继承WeWork类
     # 增加标签成员，create_labelmember_userlist方法，需传入参数tagid,userlist
     def create_labelmember_userlist(self, tagid, userlist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
+            "method": self.data["create_labelmember"]["method"],  #"post",
+            "url": self.data["create_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "userlist": userlist}
         }
@@ -87,8 +90,8 @@ class Label(WeWork): # 继承WeWork类
     # 增加标签成员，create_labelmember_partylist方法，需传入参数tagid,partylist
     def create_labelmember_partylist(self, tagid, partylist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
+            "method": self.data["create_labelmember"]["method"],  #"post",
+            "url": self.data["create_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "partylist": partylist}
         }
@@ -99,8 +102,8 @@ class Label(WeWork): # 继承WeWork类
     # 删除标签成员，delete_labelmember_all方法，需传入参数tagid,userlist,partylist
     def delete_labelmember_all(self,tagid,userlist,partylist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
+            "method": self.data["delete_labelmember"]["method"],  #"post",
+            "url": self.data["delete_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "userlist": userlist, "partylist": partylist}
         }
@@ -111,8 +114,8 @@ class Label(WeWork): # 继承WeWork类
     # 删除标签成员，delete_labelmember_userlist方法，需传入参数tagid,userlist
     def delete_labelmember_userlist(self, tagid, userlist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
+            "method": self.data["delete_labelmember"]["method"],  #"post",
+            "url": self.data["delete_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "userlist": userlist}
         }
@@ -123,8 +126,8 @@ class Label(WeWork): # 继承WeWork类
     # 删除标签成员，delete_labelmember_partylist方法，需传入参数tagid,partylist
     def delete_labelmember_partylist(self, tagid, partylist):
         req = {
-            "method": "post",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
+            "method": self.data["delete_labelmember"]["method"],  #"post",
+            "url": self.data["delete_labelmember"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers",
             "params": {"access_token": self.token},
             "json": {"tagid": tagid, "partylist": partylist}
         }
@@ -135,8 +138,8 @@ class Label(WeWork): # 继承WeWork类
     # 获取标签列表，get_labellist方法
     def get_labellist(self):
         req = {
-            "method": "get",
-            "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/list",
+            "method": self.data["get_labellist"]["method"],  #"get",
+            "url": self.data["get_labellist"]["url"],  #"https://qyapi.weixin.qq.com/cgi-bin/tag/list",
             "params": {"access_token": self.token},
         }
         r = self.send_api(req)  # 调用send_api方法，并传入一个json结构体的请求信息
--- test_label/test_case/test_file.py
+++ test_label/test_case/test_file.py
@@ -2,7 +2,24 @@ import yaml
 
 # 怕yaml文件写错，可以生成一个yaml文件
 class TestDemo:
-    def test_token(self):
+    # test_config方法生成config文件
+    def test_config(self):
+        rep = {
+            "corpid": "ww170e13c876c74789",
+            "corpsecret": "N5-mHIhmaAYP1-qcMaccISC0Ktw5L8YY93GDjUy7pjs",
+            "create_label":{"method": "post", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/create"},
+            "update_label":{"method": "post", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/update"},
+            "delete_label":{"method": "get", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/delete"},
+            "get_labelmember":{"method": "get", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/get"},
+            "create_labelmember":{"method": "post", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/addtagusers"},
+            "delete_labelmember":{"method": "post", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/deltagusers"},
+            "get_labellist":{"method": "get", "url": "https://qyapi.weixin.qq.com/cgi-bin/tag/list"}
+        }
+        with open("../data/config.yaml", "w") as f:
+            yaml.dump(rep, f)  # 把rep里的内容写到config.yaml文件中
+
+    # test_data方法生成测试数据
+    def test_data(self):
         rep = {
             "create_label_tagid":{"tagname":"Ui4", "tagid":4},
             "create_label":{"tagname":"Ui5"},
@@ -17,4 +34,4 @@ class TestDemo:
             "delete_labelmember_partylist":{"tagid":1, "partylist":[2]}
         }
         with open("../data/testdata.yaml", "w") as f:
-            yaml.dump(rep, f)  # 把rep里的内容写到config.yaml文件中
+            yaml.dump(rep, f)  # 把rep里的内容写到testdata.yaml文件中
